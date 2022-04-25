41a57f3096732dd5e036d3b54323ee845a9768bb
Lambert-Simnel <Josephsut@gmail.com>
2022-04-25 15:20:44 +0100

    Started adding testing

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+
+    private Player player;
+
+    @BeforeEach
+    public void settup() {
+        player = new Player("Stephen Kingsley", 26, 5000, "Defender", 12, 0.15);
+    }
+
+    @Test
+    void testName() {
+        assertEquals("Stephen Kingsley", player.getName());
+    }
+
+    @Test
+    void testScoring() {
+        
+    }
+}
